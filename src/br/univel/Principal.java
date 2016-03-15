74050dc6bb020bcaddd9c43ee6514384991ffa7d
Cecilia Vanin <ceciliaavanin@gmail.com>
2016-03-15 20:21:09 -0300

    Alteração a pedido do cliente.

--- src/br/univel/Principal.java
+++ src/br/univel/Principal.java
@@ -3,8 +3,8 @@ package br.univel;
 public class Principal {
 
 	public static void main(String[] args){
-		System.out.println("Olá mundo!");
-		System.out.println("Tudo bem?");
+		System.out.println("Hello Word!");
+		System.out.println("It's we");
 		}
 
 }
